f4f375f4436c4015a81df8754252e73ae17c0a3a
Sergioaero <116237665+Sergioaero1@users.noreply.github.com>
2024-10-22 00:08:40 -0300

    Upgrade
    Primeira versão

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import chalk from "chalk";
+import logSymbols from "log-symbols";
+
+
+console.log(chalk.green("Sergioaero \n"));
+console.log(logSymbols.success, chalk.yellow(" CTRL+C = ANULA O --WATCH \n"));
+
+console.log(logSymbols.error, chalk.gray(" Falhou kkkk \n"));
+console.log(logSymbols.info, chalk.blueBright(" Informação...\n"));
+console.log(logSymbols.warning, chalk.red(" Dangerous"));
